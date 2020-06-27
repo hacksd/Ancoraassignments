72316af935222e7c6c5484fbbc60f19b0de75517
farfake@outlook.com <farfake@outlook.com>
2020-06-27 05:30:20 -0500

    lab3_5 if statement

---
+++ lab35/lab35/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "Password";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam" + isSpam);
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam." + isSpam);
+            }
+        }
+    }
+}
