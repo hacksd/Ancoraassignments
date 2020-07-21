694fd56a3865efb12814458a32fd0f21598f1a27
amar hassan <hacksd@hotmail.com>
2020-07-21 11:15:23 -0500

    4_4 and 4_5 upload

---
+++ lab4_5/lab4_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the list of scores.
+            List<int> scores = new List<int>();
+            scores.Add(1200);
+            scores.Add(4400);
+            scores.Add(3700);
+            scores.Add(8900);
+            scores.Add(7200);
+
+
+            //sort the list in default order and print it.
+            Console.WriteLine("The default order: ");
+            scores.Sort();
+
+            foreach (int listItem in scores)
+            {
+                Console.WriteLine(listItem);
+            }
+
+            //Reverse the list order and print it.
+            Console.WriteLine("The reverse order: ");
+            scores.Reverse();
+
+            foreach (int listItem in scores)
+            {
+                Console.WriteLine(listItem);
+            }
+
+            //Add a new item to the list
+            scores.Add(2400);
+
+            //remove an item from the list
+            scores.Remove(4400);
+
+        }
+    }
+}
