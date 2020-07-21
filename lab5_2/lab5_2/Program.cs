70064b5113be5fae6ee8b61c63f595af35013774
amar hassan <hacksd@hotmail.com>
2020-07-21 12:08:24 -0500

    lab 5_2 working with Enum

---
+++ lab5_2/lab5_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab5_2
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+
+        }
+        static void Main(string[] args)
+        {
+            //creating a list
+            List<string> TaskList = new List<string>();
+
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)) )
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string tasks = day + " : " + Console.ReadLine();
+                TaskList.Add(tasks);
+
+            }
+            // the output foreach
+            foreach (string listitem in TaskList)
+            {
+                Console.WriteLine(listitem);
+            }
+
+        }
+    }
+}
